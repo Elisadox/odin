51799a6a463286333dbe355b28bfc176715f3aaa
Elisadox <152091133+Elisadox@users.noreply.github.com>
2025-09-16 18:02:47 +0200

    nettoyage

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const toggleDarkModeBtn = document.querySelector("#dark-mode-btn");
+const toggleLightModeBtn = document.querySelector("#light-mode-btn");
+toggleDarkModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "dark");
+});
+toggleLightModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "light");
+});
